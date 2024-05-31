4374a8b0fbc3ca0725b6d30961b0568404c2366f
Michasb04 <miesroro@gmail.com>
2024-05-30 19:23:29 -0500

    featuring: Add responsive web and added a new style to the navigation bar.

--- js/script.js
+++ js/script.js
@@ -15,12 +15,12 @@ const sr = ScrollReveal (
     }
 );
 
-/* Main Animations */
+/* Hero Animations */
 
 sr.reveal('.navlist', {delay:200, origin:'right', reset:false});
 sr.reveal('.logo-text',{delay:200,origin:'left',reset:false});
 sr.reveal('.hero-text',{delay:200,origin:'left',reset:true});
-sr.reveal('.icons',{delay:400, origin: 'bottom', reset:true});
+sr.reveal('.icons',{delay:400, origin: 'bottom', reset:false});
 sr.reveal('.hero-img',{delay:500, origin:'right'});
 
 /* Products Animations */
@@ -30,5 +30,8 @@ sr.reveal('.section-products',{delay:200, origin:'left'});
 sr.reveal('.section-contact',{delay:200,origin:'right', reset:true});
 sr.reveal('.contact-icons',{delay:200,origin:'bottom', reset:true});
 
+sr.reveal('.contact-icons',{delay:200,origin:'bottom', reset:true});
+//Footer animations
+sr.reveal('.footer',{delay:100,origin:'bottom', reset:false});
 // Show borderbox deppending to page status 
 
